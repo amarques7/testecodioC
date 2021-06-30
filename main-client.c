0ae4d6d893ac16ba1e05bca5a3e9a86098f4e407
Adriano Marcelo Marques Miyashiro <49168148+amarques7@users.noreply.github.com>
2021-06-30 16:34:59 -0400

    Add files via upload
    adição de nova variabilidade...

--- main-client.c
+++ main-client.c
@@ -35,6 +35,12 @@ int main_ForaDoEsle()
     printf("fundeu....");
     exit(0);
 
+    #ifndef TESTE_MEU_ForaElse
+    printf("sdfsklfslkf");  
+#else
+    exit("fsadfsas"); 
+#endif
+
 }
 
 #else
@@ -43,9 +49,9 @@ int main_DentroDoElse()
     printf("dsfasfa");
 
 #ifndef TESTE_MEU
-    printf("sdfsklfslkf");
+    printf("sdfsklfslkf");  
 #else
-    printf("fsadfsas");
+    exit("fsadfsas"); 
 #endif
     printf("dsfasfa");
 
