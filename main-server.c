ee380aecb9be4dc6a05a16116f3411fdf1238ecf
Adriano Marcelo Marques Miyashiro <49168148+amarques7@users.noreply.github.com>
2021-07-05 10:00:20 -0400

    Add files via upload
    troca das diretivas  #ifdef para #ifndef e inserção do #else e a inclusão das funções  printf e exit

--- main-server.c
+++ main-server.c
@@ -1,9 +1,15 @@
 #include "shared.h"
-#ifdef USE_OPENSSL
+
+#ifndef USE_OPENSSL
+
 int main_ForaDoEsle()
 {
     printf("ERROR: Not compiled. Missing OpenSSL support.\n");
     printf("sasad....");
+    exit(0);
+#else
+    printf("fdsadsfa");
+    exit(0);
 
 }
 #endif
