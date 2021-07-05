e5740d1927268cae0c13c63cb5b599ee63e96e1d
Adriano Marcelo Marques Miyashiro <49168148+amarques7@users.noreply.github.com>
2021-07-05 09:27:50 -0400

    Remoção de variabilidade e funções
    Foi removido as variabilidades e funções.
    Inicia novo ciclo de testes.

--- main-server.c
+++ main-server.c
@@ -4,29 +4,6 @@ int main_ForaDoEsle()
 {
     printf("ERROR: Not compiled. Missing OpenSSL support.\n");
     printf("sasad....");
-    exit(0);
-
-    #ifndef TESTE_MEU_ForaElse
-    printf("sdfsklfslkf");  
-#else
-    exit("fsadfsas"); 
-#endif
-
-}
-
-#else
-int main_DentroDoElse()
-{
-    printf("dsfasfa");
-
-#ifndef TESTE_MEU
-    printf("sdfsklfslkf");
-    printf("sdfsklfslkf");  
-#else
-    printf("fsadfsas");
-    exit("fsadfsas"); 
-#endif
-    printf("dsfasfa");
 
 }
 #endif
