45a517c528926592b360ab669e0d7a150b8aaa3f
Adriano Marcelo Marques Miyashiro <49168148+amarques7@users.noreply.github.com>
2021-07-21 08:24:55 -0400

    Add files via upload
    criação de casos de teste para validar o scritp de validação.

--- main-server.c
+++ main-server.c
@@ -2,24 +2,38 @@
 
 #ifdef USE_OPENSSL
 
-int main_ForaDoEsle()
-{
-    printf("ERROR: Not compiled. Missing OpenSSL support.\n");
+int main_ForaDoEsle(){
     printf("sasad....");
+    va_start(p);
     exit(0);
+    va_end(0);
 #ifndef USE_PPPPP
     printf("fdfa");
     open("fasdf.tx");
 #else
-print("asdfasfa"); 
-close("dafsd.tx");
+    print("asdfasfa"); 
+    close("dafsd.tx");
+    va_end(p);
 #endif
  }  
 #else
 
 int main_DentroDoElse(){
-     printf("fdsadsfa");
+    calloc(P);
+    printf("fdsadsfa");
     exit(0);
 
 }
 #endif
+int funcao1(){
+    fopen("C:\meuArquivo.txt");
+    memcpy(v);
+    free(x);
+
+#ifdef TESTEVARI
+ malloc(c);
+ strlen(d);
+#endif
+ strcpy(x);
+ fclose(x);
+}
