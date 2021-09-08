9b787fe05d100bb4573b4c740289e525d2f86c93
Adriano Marcelo Marques Miyashiro <49168148+amarques7@users.noreply.github.com>
2021-09-08 14:35:05 -0400

    Add files via upload
    teste diferença do git

--- main-server.c
+++ main-server.c
@@ -2,38 +2,14 @@
 
 #ifdef USE_OPENSSL
 
-int main_ForaDoEsle(){
+int main_ForaDoEsle()
+{
+    printf("ERROR: Not compiled. Missing OpenSSL support.\n");
     printf("sasad....");
-    va_start(p);
     exit(0);
-    va_end(0);
-#ifndef USE_PPPPP
-    printf("fdfa");
-    open("fasdf.tx");
 #else
-    print("asdfasfa"); 
-    close("dafsd.tx");
-    va_end(p);
-#endif
- }  
-#else
-
-int main_DentroDoElse(){
-    calloc(P);
     printf("fdsadsfa");
     exit(0);
 
 }
 #endif
-int funcao1(){
-    fopen("C:\meuArquivo.txt");
-    memcpy(v);
-    free(x);
-
-#ifdef TESTEVARI
- malloc(c);
- strlen(d);
-#endif
- strcpy(x);
- fclose(x);
-}
